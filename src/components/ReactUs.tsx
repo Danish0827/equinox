53fb0e6fba42dcfbb2ddb3067b867dfb5737bafe
danish <s.danish0827@email.com>
2025-05-23 00:01:50 +0530

    animation updated

--- src/components/ReactUs.tsx
+++ src/components/ReactUs.tsx
@@ -18,7 +18,7 @@ const ReactUs = () => {
           el,
           { opacity: 0, y: 40 },
           {
-            delay : 1.5,
+            delay : 1,
             opacity: 1,
             y: 0,
             duration: 1,
